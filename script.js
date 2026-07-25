5147ad06e72ff4903fd71ebca01b6562cb2e04fd
sakshi210108 <kartavya924@gmail.com>
2026-07-25 18:00:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,27 @@
-function openGift() {
+const output=document.getElementById("output");
 
-    const gift = document.getElementById("gift");
-    const message = document.getElementById("message");
+fetch("art.txt")
 
-    gift.classList.add("open");
+.then(r=>r.text())
 
-    setTimeout(() => {
+.then(text=>{
 
-        message.classList.remove("hidden");
-        createConfetti();
+let i=0;
 
-    }, 800);
+function type(){
 
-}
+if(i<text.length){
 
+output.textContent+=text.charAt(i);
 
+i++;
 
-function createConfetti() {
-
-    const confettiBox = document.getElementById("confetti");
-
-    for (let i = 0; i < 150; i++) {
-
-        const confetti = document.createElement("div");
-
-        confetti.classList.add("confetti");
-
-        confetti.style.left = Math.random() * 100 + "vw";
-        confetti.style.animationDelay = Math.random() * 3 + "s";
-        confetti.style.backgroundColor =
-            "hsl(" + Math.random() * 360 + ",100%,50%)";
-
-        confettiBox.appendChild(confetti);
-
-    }
+setTimeout(type,1);
 
 }
 
+}
 
-// Try to start music automatically
-window.addEventListener("load", () => {
-
-    const music = document.getElementById("birthdaySong");
-
-    music.volume = 0.7;
-
-    music.play().catch(() => {
-        console.log("Browser blocked autoplay.");
-    });
+type();
 
 });
